5b1104501f02be063bdcf5a807ade64c713d3004
DevonGriffith <griffith.devon@gmail.com>
2017-03-16 20:10:54 -0400

    Create AppDelegate.swift

---
+++ AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Private Communicator
+//
+//  Created by Devon Griffith on 2017-03-16.
+//  Copyright © 2017 Devon Griffith. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
