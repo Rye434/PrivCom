6297574e788a434a7a3dbeb6dade25669a25c651
DevonGriffith <griffith.devon@gmail.com>
2017-03-16 20:16:11 -0400

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Private Communicator
+//
+//  Created by Devon Griffith on 2017-03-16.
+//  Copyright © 2017 Devon Griffith. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
